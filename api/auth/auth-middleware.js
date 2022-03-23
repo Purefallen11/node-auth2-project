c4958cfb935c1f41f6e1a2b789652063260a11a5
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-03-23 14:18:45 -0500

    done with auth-middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const jwt = require('jsonwebtoken')
 const Users = require('../users/users-model');
-const { options } = require("./auth-router");
 
 const restricted = (req, res, next) => {
   /*
@@ -53,7 +52,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -61,6 +60,18 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
+  try {
+    const [user] = await Users.findBy({ username: req.body.username })
+    if (!user) {
+      next({status: 401, message: "invalid credentials"})
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
+  
 }
 
 
@@ -83,6 +94,17 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
+  if (!req.body.role_name || !req.body.role_name.trim()) {
+    req.role_name = 'student'
+    next()
+  } else if (req.body.role_name.trim() === 'admin') {
+    next({status: 422, message: 'Role name can not be admin'})
+  } else if (req.body.role_name.trim().length > 32) {
+    next({status: 422, message: "Role name can not be longer than 32 chars"})
+  } else {
+    req.role_name = req.body.role_name.trim()
+    next()
+  }
 }
 
 module.exports = {
